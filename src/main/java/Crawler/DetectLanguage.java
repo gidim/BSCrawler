07f2eebe08679a81dbb07642b4e118697a03ce57
Gideon Mendels <gideonm@gmail.com>
2014-11-29 20:17:02 -0500

    again

--- src/main/java/Crawler/DetectLanguage.java
+++ src/main/java/Crawler/DetectLanguage.java
@@ -32,7 +32,7 @@ public class DetectLanguage {
         Random rand = new Random();
         String fileLocation = tempFile + String.valueOf(id);
 
-
+g
         //String path = DetectLanguage.class.getProtectionDomain().getCodeSource().getLocation().getPath();
         //String decodedPath = URLDecoder.decode(path, "UTF-8").replace("/target/classes/","").replace("file:","").replace("/home/gm2597/crawler/BSCrawler/target/BSCrawler-1.0-SNAPSHOT-jar-with-dependencies.jar/langDetect.py","langDetect.py");
         String decodedPath = "/home/gm2597/crawler/BSCrawler/"; //production
